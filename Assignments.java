1afa2749d6f032f644c8fb7b3e9d56d7c08398fa
AzizAmaanAhmed887 <amanahmed1122004@gmail.com>
2026-08-26 20:43:14 +0530

    MinCost

--- Assignments.java
+++ Assignments.java
@@ -1,9 +1,10 @@
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Assignments {
-    public static void reverseFirstK(Queue<Integer> queue, int k) {
+    static void reverseFirstK(Queue<Integer> queue, int k) {
         Stack<Integer> st = new Stack<>();
 
         // 1. Remove first k els
@@ -24,7 +25,7 @@ public class Assignments {
         }
     }
 
-    public static void generateBinary(int N) {
+    static void generateBinary(int N) {
         Queue<String> q = new LinkedList<>();
 
         q.add("1");
@@ -34,20 +35,47 @@ public class Assignments {
 
             System.out.print(curr + " ");
 
-
             q.add(curr + '0');
             q.add(curr + "1");
         }
     }
 
+    static int minCost(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
+        }
+
+        int cost = 0;
+
+        while (pq.size() > 1) {
+            int a = pq.remove();
+            int b = pq.remove();
+
+            int sum = a + b;
+
+            cost += sum;
+
+            pq.add(sum);
+        }
+
+        return cost;
+    }
+
     public static void main(String[] args) {
-        int N = 5;
-        generateBinary(N);
+
+        int[] arr = { 4, 3, 2, 6 };
+
+        System.out.println(minCost(arr)); // Calculates Minimum cost
+
+        // int N = 5;
+        // generateBinary(N);
 
         // Queue<Integer> q = new LinkedList<>();
 
         // for (int i = 10; i <= 100; i += 10) {
-        //     q.add(i);
+        // q.add(i);
         // }
 
         // int k = 5;
